8938e4cbf1212a98ebf4fbfd5471aa6db3730176
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2022-12-08 12:51:47 +0200

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 3/step_3.js
@@ -0,0 +1,8 @@
+const text = document.querySelector('input');
+const value = document.getElementById('value');
+
+text.addEventListener('input', updateValue);
+
+function updateValue(a){
+    value.textContant=a.target.value;
+}
