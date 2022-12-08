8eb5517ccc6a4b9d500874684f8bb710f5a7b815
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2022-12-08 23:02:25 +0200

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,13 @@
+function colorGreen() {
+    document.getElementById("text").style.color = "green";
+  }
+  
+  function colorRed() {
+    document.getElementById("text").style.color = "red";
+  }
+  
+  function colorBlue() {
+    document.getElementById("text").style.color = "blue";
+  }
+  
+  
