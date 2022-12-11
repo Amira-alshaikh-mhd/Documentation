0ee1e0255b5d7628c604e6d7dd46ea8890c890a5
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2022-12-11 23:43:27 +0200

    Creating Table

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -5,3 +5,28 @@ SELECT   Points  Alex FROM students;
  INSERT INTO students(ID, Name, Age, Gender, Points) VALUES (20, 'Amira', 34, 'F', 300);
  UPDATE students SET Points = 350 WHERE name = 'basma';
  UPDATE students SET Points = 150 WHERE name = 'Alex';
+ 
+ 
+ 
+  10 - CREATE TABLE graduates (
+ID INT NOT NULL PRIMARY KEY,
+Name TEXT NOT NULL UNIQUE,
+Age INT,
+Gender TEXT,
+Points INT,
+Graduation TEXT
+);
+
+
+  11- INSERT INTO graduates (ID, Name, Age, Gender, Points)
+      SELECT * FROM students
+      WHERE name='Layla';
+      
+  12- UPDATE graduates
+    SET Graduation='08/09/2018'
+    WHERE Name='Layal';
+    
+  13- DELETE FROM students WHERE Name='Layal';
+       
+
+
