c47e781427644b6951d389c526d4c57878909e54
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2023-01-07 01:41:31 +0200

    step 7

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -6,7 +6,12 @@ const port = 3000
 
 
 
-const movies = [{ title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }]
+const movies = [
+    {id:1, title: 'Jaws', year: 1975, rating: 8 },
+    {id:2, title: 'Avatar', year: 2009, rating: 7.8 },
+    {id:3, title: 'Brazil', year: 1985, rating: 8 },
+    {id:4, title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
 
 
 
@@ -105,6 +110,27 @@ app.get('/movies/read/by-title', (req, res) => {
     res.status(200);
     res.send( sortedMovies);
   });
+
+
+
+//   movie defined by the ID
+
+  app.get('/movies/read/id/:id', (req, res) => {
+    
+    const id = req.params.id;
+
+    const movie = movies.find(movie => movie.id === Number(id));
+
+    if (movie) {
+      res.status(200);
+      res.send(movie)
+    } else {
+      res.status(404);
+      res.send(`the movie ${id} does not exist` );
+    }
+  });
+
+
 
 
 // update command
