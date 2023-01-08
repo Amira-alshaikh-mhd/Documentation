41394ebb6d27d99d3768bda3a9a2429d5ef60a15
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2023-01-08 19:54:23 +0200

    step 8

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -7,10 +7,10 @@ const port = 3000
 
 
 const movies = [
-    {id:1, title: 'Jaws', year: 1975, rating: 8 },
-    {id:2, title: 'Avatar', year: 2009, rating: 7.8 },
-    {id:3, title: 'Brazil', year: 1985, rating: 8 },
-    {id:4, title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+    { id: 1, title: 'Jaws', year: 1975, rating: 8 },
+    { id: 2, title: 'Avatar', year: 2009, rating: 7.8 },
+    { id: 3, title: 'Brazil', year: 1985, rating: 8 },
+    { id: 4, title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
 ]
 
 
@@ -108,27 +108,27 @@ app.get('/movies/read/by-rating', (req, res) => {
 app.get('/movies/read/by-title', (req, res) => {
     const sortedMovies = movies.sort((a, b) => a.title.localeCompare(b.title));
     res.status(200);
-    res.send( sortedMovies);
-  });
+    res.send(sortedMovies);
+});
 
 
 
 //   movie defined by the ID
 
-  app.get('/movies/read/id/:id', (req, res) => {
-    
+app.get('/movies/read/id/:id', (req, res) => {
+
     const id = req.params.id;
 
     const movie = movies.find(movie => movie.id === Number(id));
 
     if (movie) {
-      res.status(200);
-      res.send(movie)
+        res.status(200);
+        res.send(movie)
     } else {
-      res.status(404);
-      res.send(`the movie ${id} does not exist` );
+        res.status(404);
+        res.send(`the movie ${id} does not exist`);
     }
-  });
+});
 
 
 
@@ -147,6 +147,31 @@ app.get('/movies/delete', (req, res) => {
 
 
 
+
+
+
+
+
+app.get('/movies/add', (req, res) => {
+    const { id, title, year, rating } = req.query
+    const movie = {
+        id: movies[movies.length - 1].id + 1,
+        title,
+        year: Number(year),
+        rating: rating || 4,
+    };
+    if (!title || !year) {
+        res.status(403)
+        res.send("you cannot create a movie wiyhout providing a title and a year")
+
+    }
+    if (isNaN(year), year.length !== 4) {
+        res.status(403)
+        res.send("you cannot create a movie wiyhout providing a title and a year")
+    }
+    movies.push(movie);
+    res.send(movies);
+})
 
 
 
